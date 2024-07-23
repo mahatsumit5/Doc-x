4180b3284105e9fd9f260108462405119430da12
Sumit Mahat <mahatsumit5@gmail.com>
2024-07-23 20:33:59 +1000

    project initialized

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import "./globals.css";
+import { Inter as FontSans } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+import { Metadata } from "next";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+export const metadata: Metadata = {
+  title: "Doc X",
+  description: "Collaborative live text editor",
+};
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
